e41fe87a8b9eac5715fb3b0c91920b7f32ce8f1c
HIEP <hiephoang.nguyen.hcm@gmail.com>
2018-08-26 15:08:06 +0700

    Add Services code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -15,6 +15,7 @@ import { MainLayoutComponent } from './layouts/main-layout/main-layout.component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule,
     CoreModule
---
+++ src/app/core/data-models/Token.ts
@@ -0,0 +1,66 @@
+export interface IToken {
+    access_token: string;
+    token_type: string;
+    refresh_token: string;
+    expires_in: number;
+    scope: string;
+}
+
+export class Token {
+    private _access_token: string;
+    private _token_type: string;
+    private _refresh_token: string;
+    private _expires_in: number;
+    private _scope: string;
+
+    constructor(token?: IToken) {
+        if (token) {
+            this.access_token = token.access_token;
+            this.token_type = token.token_type;
+            this.refresh_token = token.refresh_token;
+            this.expires_in = token.expires_in;
+            this.scope = token.scope;
+        } else {
+            this.access_token = "";
+            this.token_type = "";
+            this.refresh_token = "";
+            this.expires_in = 0;
+            this.scope = "";
+        }
+    }
+
+    public get access_token(): string {
+        return this._access_token;
+    }
+    public set access_token(value: string) {
+        this._access_token = value;
+    }
+
+    public get token_type(): string {
+        return this._token_type;
+    }
+    public set token_type(value: string) {
+        this._token_type = value;
+    }
+
+    public get refresh_token(): string {
+        return this._refresh_token;
+    }
+    public set refresh_token(value: string) {
+        this._refresh_token = value;
+    }
+
+    public get expires_in(): number {
+        return this._expires_in;
+    }
+    public set expires_in(value: number) {
+        this._expires_in = value;
+    }
+
+    public get scope(): string {
+        return this._scope;
+    }
+    public set scope(value: string) {
+        this._scope = value;
+    }
+}
