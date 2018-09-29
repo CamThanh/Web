b5cb41912f9b223eb6fc2b97e845c22c38eb482a
HIEP <hiephoang.nguyen.hcm@gmail.com>
2018-09-29 19:49:22 +0700

    Change Token Model

--- src/app/core/data-models/Token.ts
+++ src/app/core/data-models/Token.ts
@@ -1,12 +1,12 @@
+import { IUser, User } from "./User";
+
 export interface IToken {
   access_token: string;
   token_type: string;
   refresh_token: string;
   expires_in: number;
   scope: string;
-  avatarUri: string;
-  email: string;
-  username: string;
+  user: IUser;
 }
 
 export class Token {
@@ -15,9 +15,7 @@ export class Token {
   refresh_token: string;
   expires_in: number;
   scope: string;
-  avatarUri: string;
-  email: string;
-  username: string;
+  user: User;
 
   constructor(token?: IToken) {
     if (token) {
@@ -26,18 +24,14 @@ export class Token {
       this.refresh_token = token.refresh_token;
       this.expires_in = token.expires_in;
       this.scope = token.scope;
-      this.avatarUri = token.avatarUri;
-      this.email = token.email;
-      this.username = token.username;
+      this.user = token.user;
     } else {
       this.access_token = "";
       this.token_type = "";
       this.refresh_token = "";
       this.expires_in = 0;
       this.scope = "";
-      this.avatarUri = "";
-      this.email = "";
-      this.username = "";
+      this.user = new User();
     }
   }
 }
